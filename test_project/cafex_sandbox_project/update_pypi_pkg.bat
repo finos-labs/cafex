c3b390f48905e4af1b33e56e1512fbb0812cec02
Sandeep Arora <Sandeep.Arora@spglobal.com>
2025-09-23 17:52:34 +0530

    Cafex Test Project | Deleted Obselete files and Updated few files Signed-off-by: Sandeep-Arora_spglobal <sandeep.arora@spglobal.com>

--- test_project/cafex_sandbox_project/update_pypi_pkg.bat
+++ test_project/cafex_sandbox_project/update_pypi_pkg.bat
@@ -1,3 +1,3 @@
-pip uninstall cafex,cafex-api,cafex-db,cafex-ui -y
+pip uninstall cafex,cafex-api,cafex-db,cafex-ui,cafex-desktop -y
 pip install cafex==0.0.50 --index https://<username>:<pat_token>@spglobal.pkgs.visualstudio.com/_packaging/MI/pypi/simple/ --extra-index-url https://pypi.org/simple
 pause
